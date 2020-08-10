288c7fb9e0ed3a47bf7f97185df173e7ae1d0117
Noah Eakin <noaheakin@gmail.com>
2020-08-10 17:20:50 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -0,0 +1,15 @@
+def new_hash
+  hash = {}
+end
+
+def my_hash
+  hash = {:sport => "soccer"}
+end
+
+def pioneer
+  hash = {name: "Grace Hopper"}
+end
+
+def id_hash_generator (integer)
+  hash = {id: integer}
+end
